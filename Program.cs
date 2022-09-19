73c548f113d67cbf3ffa460cffeab34495f1e49b
DzhangrErdniev21D <85980107+KalmDzhan@users.noreply.github.com>
2022-09-19 12:14:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ErdnievDzhangr21D_1._43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum, secondNum, average, geometricMean;
+
+            Console.WriteLine("Эта программа рассчитывает среднее арифметическое и среднее геометрическое ваших чисел");
+
+            Console.Write("Введи первое число: ");
+            firstNum = double.Parse(Console.ReadLine());
+
+            Console.Write("Введи второе число: ");
+            secondNum = double.Parse(Console.ReadLine());
+
+            average = (firstNum + secondNum) / 2;
+            geometricMean = Math.Round(Math.Sqrt(firstNum * secondNum), 2);
+
+            Console.WriteLine("Среднее арифметическое {0}", average);
+            Console.WriteLine("Среднее геометрическое {0}", geometricMean);
+
+
+        }
+    }
+}
